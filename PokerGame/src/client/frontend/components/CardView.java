2ad38c4944879f8a749ff11f6000690e13efcdf3
Máté Sárdi <mate.sardi@realeyesit.com>
2018-05-06 19:09:21 +0200

    optimalizáció: a CardView most már előre betölti a memóriába az összes kártya képét

--- PokerGame/src/client/frontend/components/CardView.java
+++ PokerGame/src/client/frontend/components/CardView.java
@@ -1,45 +1,60 @@
 package client.frontend.components;
 
+import model.Card;
+import model.Deck;
 import model.Rank;
 import model.Suit;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class CardView extends JLabel {
 
-    // TODO preload all card images
-    private BufferedImage image;
+    private static Map<Card, Image> images = new HashMap<>();
+    private static Image backImage;
+
+    private static Image loadAndResizeImage(String path) {
+        File input = new File(path);
+        Image scaledImage = null;
+        try {
+            BufferedImage image = ImageIO.read(input);
+            scaledImage = image.getScaledInstance(80, 120, Image.SCALE_SMOOTH);
+        } catch (IOException e) {
+            System.out.println("Could not find card image at " + path);
+            e.printStackTrace();
+        }
+        return scaledImage;
+    }
+
+    static {
+        Deck deck = new Deck();
+        Stream.generate(deck::getTopCard).limit(52).forEach(card -> {
+            String path;
+            Rank rank = card.getRank();
+            Suit suit = card.getSuit();
+            path = "./src/client/frontend/resources/" + ("" + rank.asChar()).replace("T", "10") + suit.toString().substring(0, 1) + ".png";
+            Image image = loadAndResizeImage(path);
+            images.put(card, image);
+        });
+
+        String path = "./src/client/frontend/resources/red_back.png";
+        backImage = loadAndResizeImage(path);
+    }
 
     public CardView(model.Card card) {
         super();
-        try {
-            String path;
-            if (card != null) {
-                Rank rank = card.getRank();
-                Suit suit = card.getSuit();
-                // TODO remove
-                path = "./src/client/frontend/resources/" + ("" + rank.asChar()).replace("T", "10") + suit.toString().substring(0, 1) + ".png";
-            } else {
-                path = "./src/client/frontend/resources/red_back.png";
-            }
-            System.out.println(path);
-            File input = new File(path);
-            System.out.println(input.exists());
-            image = ImageIO.read(input);
-        } catch (IOException ex) {
-        }
         setSize(80, 120);
-        setIcon(new ImageIcon(image.getScaledInstance(80, 120, java.awt.Image.SCALE_SMOOTH)));
+        if (card != null) {
+            setIcon(new ImageIcon(images.get(card)));
+        } else {
+            setIcon(new ImageIcon(backImage));
+        }
     }
-
-//    @Override
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-//        g.drawImage(image, 0, 0, 80, 120, this); // see javadoc for more info on the parameters
-//    }
-
 }
